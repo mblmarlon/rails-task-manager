015d5c0edb6230988e1376ca4cc1d84ef8046817
Marlon Berdefy <berdefymarlon@yahoo.de>
2017-08-16 10:34:51 +0200

    task manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: "eat Chicken", description: "faszkivan")
+Task.create(name: "play soccer", description: "sports")
+Task.create(name: "go to sleep", description: "important stuff")
+Task.create(name: "facetime", description: "why?")
